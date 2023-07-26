816f944e8a8442382e34883f672530750056620c
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-27 04:37:52 +0530

    Extended

--- src/main/java/ChatClient.java
+++ src/main/java/ChatClient.java
@@ -4,24 +4,21 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
-public class ChatClient extends UnicastRemoteObject implements ChatClientInterface {
+public class ChatClient extends UnicastRemoteObject  {
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 1099;
 
     protected ChatClient() throws RemoteException {
     }
 
-    @Override
-    public void receiveMessage(String message) {
-        System.out.println(message);
-    }
 
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry(SERVER_HOST, SERVER_PORT);
             ChatService server = (ChatService) registry.lookup("ChatService");
 
-            ChatClient client = new ChatClient();
+            User client = new User();
+            client.name="Bandara";
             server.registerClient(client);
 
             Scanner scanner = new Scanner(System.in);
